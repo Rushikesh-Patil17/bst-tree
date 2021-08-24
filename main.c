6272af93319f86e2d0cf9291c39d08d7d95e3972
Sakshi2317 <sakshi.pawar1710@gmail.com>
2021-08-24 23:28:49 +0530

    Added driver code

--- main.c
+++ main.c
@@ -4,5 +4,24 @@
 int main() {
     tree t;
     init(&t);
+    
+    insert(&t, 12);
+    insert(&t, 28);
+    insert(&t, 38);
+    insert(&t, 48);
+    insert(&t, 1);
+    insert(&t, 8);
+    insert(&t, 15);
+    insert(&t, 45);
+    insert(&t, 50);
+    
+    inorder_traverse(t);
+    
+    removeNode(&t, 8);
+    removeNode(&t, 45);
+    
+    preorder_traverse(t);
+    postorder_traverse(t);
+    
     return 0;
 }
